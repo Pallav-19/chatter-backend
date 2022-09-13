856d3330c59a2ba0256fae1120ff93485197099a
Pallav <pallavinvizag@gmail.com>
2022-09-13 21:24:16 +0530

    MVC set-up

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const port = process.env.PORT;
+app.get("/", (req, res) => {
+  res.send("welcome to chatter!");
+});
+app.listen(port, () => {
+  console.log(`listening from port ${port}`);
+  console.log(`open at http://localhost:${port}`);
+});
