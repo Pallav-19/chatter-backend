77a7f4a6af4ff5d5a115ce2d964856e5fe0ccd92
Pallav <pallavinvizag@gmail.com>
2022-09-13 23:17:29 +0530

    database configuration

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
+const dbconfig = require("./database.config.js");
+
+
 app.get("/", (req, res) => {
   res.send("welcome to chatter!");
 });
+
+
+
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`listening from port ${port}`);
   console.log(`open at http://localhost:${port}`);
+  dbconfig();
 });
