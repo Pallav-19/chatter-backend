7d1c3e723f4e5534bead7899803915e98612d60f
Pallav <pallavinvizag@gmail.com>
2022-09-14 14:23:45 +0530

    auth routes

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const dbconfig = require("./database.config.js");
+const CORS = require("cors");
+const colors = require("colors");
+const userRoutes = require("./routes/user.router.js");
+app.use(CORS());
 
-
-app.get("/", (req, res) => {
-  res.send("welcome to chatter!");
-});
-
+app.use("/api/user", userRoutes);
 
 
 const port = process.env.PORT;
 app.listen(port, () => {
-  console.log(`listening from port ${port}`);
-  console.log(`open at http://localhost:${port}`);
+  console.log(`listening from port ${port}`.yellow);
+  console.log(`open at http://localhost:${port}`.underline.cyan);
   dbconfig();
 });
